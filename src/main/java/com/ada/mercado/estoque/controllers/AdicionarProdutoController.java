6ec8eaffff6aba76f83f64ac2254edac13ac41fc
Eulália Ribeiro <eulaliaribeiroecr@gmail.com>
2023-03-07 23:14:47 -0300

    atualizando AdicionarProdutoController

--- src/main/java/com/ada/mercado/estoque/controllers/AdicionarProdutoController.java
+++ src/main/java/com/ada/mercado/estoque/controllers/AdicionarProdutoController.java
@@ -1,30 +1,37 @@
 package com.ada.mercado.estoque.controllers;
 
-import com.ada.mercado.estoque.entities.Produto;
+import com.ada.mercado.estoque.dtos.ProdutoDTO;
+import com.ada.mercado.estoque.producers.ProdutoProducer;
 import lombok.RequiredArgsConstructor;
+import lombok.var;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/produto")
 @RequiredArgsConstructor
 public class AdicionarProdutoController {
 
-    @PostMapping(path = "/add")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void realizarPagamentoFornecedor(){
+    private final ProdutoProducer produtoProducer;
 
-    }
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> sendMessage(@RequestBody ProdutoDTO produtoDTO){
+        var id = UUID.randomUUID().toString();
 
-    @GetMapping(path = "/find/{id}")
-    public void getProdutoByIdentificador(@PathVariable("id") String identificador){
+        var message = com.ada.mercado.Produto.newBuilder()
 
-    }
+                .setIdProduto(id)
+                .setNome(produtoDTO.getNome())
+                .setQuantidade(produtoDTO.getQuantidade())
+                .setPreco(produtoDTO.getPreco())
+                .build();
+        produtoProducer.sendMessage(message);
 
-    @GetMapping(path = "")
-    public void getAllProdutos(){
-
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 
